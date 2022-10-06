81a1d7d3b97afc812421c3fdfac0c11c1572578c
EduCabreraMendoza <edu.cabrera.mendoza@gmail.com>
2022-10-06 14:34:22 -0500

    CAbio de ubicacion

--- MySQL/query_cmd_query.py
+++ MySQL/query_cmd_query.py
@@ -3,7 +3,7 @@ import mysql.connector
 cnx = mysql.connector.connect(user='lalo', password='2907', host='127.0.0.1', database='codigoIoT')
 #cursor = cnx.cursor()
 
-query = ("SELECT * FROM historicoAPI;")
+query = ("SELECT id,nombre,temperatura FROM historicoAPI WHERE id=1;")
 
 #cursor.execute(query)
 res = cnx.cmd_query(query)
---
+++ MySQL/query_cursor_insert.py
@@ -0,0 +1,20 @@
+#Bibliotecas
+import mysql.connector
+
+#Conexión
+cnx = mysql.connector.connect(user='lalo', password='2907', host='127.0.0.1', database='codigoIoT')
+
+#Query
+query_insert = ("INSERT INTO rfid (nombre,texto,rfid) VALUES ('Lalo','Prueba Python 2',872589727);")
+
+#Petición
+cursor = cnx.cursor()
+cursor.execute(query_insert)
+
+#Asegurarse de que la incercion de datos fue correcta
+cnx.commit()
+print("Query OK")
+
+#Final de la conexión
+cursor.close()
+cnx.close()
