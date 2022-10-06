3b540190b606c49625c8244afacbb43736bd4eaf
EduCabreraMendoza <edu.cabrera.mendoza@gmail.com>
2022-10-06 12:17:46 -0500

    Cambio de nombre

--- query_cmd_query.py
+++ query_cmd_query.py
@@ -3,7 +3,7 @@ import mysql.connector
 cnx = mysql.connector.connect(user='lalo', password='2907', host='127.0.0.1', database='codigoIoT')
 #cursor = cnx.cursor()
 
-query = ("SELECT id,nombre,temperatura FROM historicoAPI WHERE nombre=\"Lalo\";")
+query = ("SELECT * FROM historicoAPI;")
 
 #cursor.execute(query)
 res = cnx.cmd_query(query)
