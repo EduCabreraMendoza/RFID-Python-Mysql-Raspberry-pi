509ea83b1f1a31d0dda2e258ac0be2e0b6761919
EduCabreraMendoza <edu.cabrera.mendoza@gmail.com>
2022-10-11 14:01:03 -0500

    Borrado de archivo de prueba

--- prueba.py
+++
@@ -1,8 +0,0 @@
-from numpy import double
-
-
-nombre = input("Ingresa tu nombre: ")
-texto = input("Ingresa tu texto: ")
-id = input("Ingresa tu id: ")
-
-print ("INSERT INTO rfid (nombre,texto,rfid) VALUES ('"+nombre+"','"+texto+"',"+id+");")
